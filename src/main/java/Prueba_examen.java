8053233012977223c59b5b30e15bf4c11011dc67
EduJimCue <edu95tecno@gmail.com>
2022-05-01 12:24:11 +0200

    prueba de creacion nuevo repositorio de git

---
+++ src/main/java/Prueba_examen.java
@@ -0,0 +1,20 @@
+public class Prueba_examen {
+    private int Numeritos;
+    private String Cadena;
+
+    public int getNumeritos() {
+        return Numeritos;
+    }
+
+    public void setNumeritos(int numeritos) {
+        Numeritos = numeritos;
+    }
+
+    public String getCadena() {
+        return Cadena;
+    }
+
+    public void setCadena(String cadena) {
+        Cadena = cadena;
+    }
+}
